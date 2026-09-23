c28e6699a2c0fb98be3d7b0462cf38b44d6b9137
Shaswat Raj <shaswatraj3@gmail.com>
2026-09-23 22:41:56 +0530

    fix: import link for popular book cards

--- app/(books)/books/page.tsx
+++ app/(books)/books/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import BookLibrary from "../../components/BookLibrary";
 import { getBookCatalog } from "../../../lib/book-repository";
 
